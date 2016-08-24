5058dfdd385778614487cf150083d12bd4b70a20
root <root@workstation01.example.com>
2016-08-24 22:38:17 +0000

    added scripts

---
+++ scripts/prepare_ansible_server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# this script prepares ansible server to deploy the infrastructure
+
+yum -y instal epel-relase -y
+yum -y install git ansible -y
+cd /root
+git clone https://github.com/Li9onGitHub/DevOpsDemo.git
+cp -rf /root/DevOpsDemo/ansible/* /etc/ansible/
+mkdir /etc/ansible/keys/
+mkdir /etc/ansible/license/
+echo "!!!!NOTE!!!"
+echo "Before you begin you must:"
+echo
+echo "################ SSH KEY ##################"
+echo "Please be aware that SSH private key should be copied to /etc/ansible/keys/devops.key"
+echo "Do not forget to configure proper permissions: # chmod 0600 /etc/ansible/keys/devops.key"
+echo
+echo "################ Chef Automate license file ##############"
+echo "Chef Automate license file should be copied to /etc/ansible/license/automate.license"
+echo
+
