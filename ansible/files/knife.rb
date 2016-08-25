57ba6323034563abd1377b9499e13e6fa5f6836a
root <root@workstation01.example.com>
2016-08-25 17:50:23 +0000

    added chef node bootstraping

--- ansible/files/knife.rb
+++ ansible/files/knife.rb
@@ -3,7 +3,8 @@ log_level                :info
 log_location             STDOUT
 node_name                'chefroot'
 client_key               "#{current_dir}/chefroot.pem"
-validation_client_name   'chef-validator'
+#validation_client_name   'chef-validator'
+validation_client_name   'demoorg-validator'
 validation_key           "#{current_dir}/demoorg-validator.pem"
 chef_server_url          'https://chefserver01.example.com/organizations/demoorg'
 cache_type               'BasicFile'
